af9212556bee24160c728e80c22d41d868a5fb4f
matt <matt@madeye.com>
2013-06-22 11:03:15 +0100

    Added some tests around coverting to/from unix time

--- test/com/madeye/clojure/common/common_test.clj
+++ test/com/madeye/clojure/common/common_test.clj
@@ -1,12 +1,29 @@
 (ns com.madeye.clojure.common.common-test
   (:require [clojure.test :refer :all]
+            [clj-time.core :as tm]
             [com.madeye.clojure.common.common :refer :all]))
 
-(deftest propstest
+(deftest test-load-props
   (testing "Loading properties from test/test.properties"
     (let [props (load-props "test/test.properties")]
       (is (= 1 (count props)))
       (is (= "test.value" (:test.property props)))
+    )
+  )
+)
+
+(deftest test-to-unix-time
+  (testing "Testing conversion of time to Unix time"
+    (let [unixtime (to-unix-time (tm/date-time 2013 6 22 10 56))]
+      (is (= 1371898560 unixtime))
+    )
+  )
+)
+
+(deftest test-from-unix-time
+  (testing "Testing conversion of time from Unix time"
+    (let [normaltime (from-unix-time 1371898560)]
+      (is (=  (tm/date-time 2013 6 22 10 56) normaltime))
     )
   )
 )
