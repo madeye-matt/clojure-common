46e90ec6fce8ea99b33717c708c0e924f7b297c3
Matt Innes <matt@madeye.com>
2014-11-18 14:00:12 +0000

    Updated version prior to release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.madeye.clojure.common/common "0.2.4-SNAPSHOT"
+(defproject com.madeye.clojure.common/common "0.2.4"
   :description "Common utility code to be shared between clojure projects"
   :url "https://github.com/madeye-matt/clojure-common"
   :license {:name "Eclipse Public License"
